1a9ec19c59c0e962a77db754fb72090add89d25a
Tweekz <andreipetrea50@gmail.com>
2023-03-17 20:06:50 +0200

    Add operate function that takes in an operator and two numbers

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(op, a, b) {
+    switch (op) {
+        case '+':
+            add(a, b);
+            break;
+        case '-':
+            subtract(a, b);
+            break;
+        case '*':
+            multiply(a, b);
+            break;
+        case '/':
+            divide(a, b);
+            break;
+        default:
+            break;
+    }
+}
