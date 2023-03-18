6f7a583c9a03d1d360d8434b570381153f4b3100
Tweekz <andreipetrea50@gmail.com>
2023-03-18 09:29:53 +0200

    Add function to calculate

--- script.js
+++ script.js
@@ -17,26 +17,83 @@ function divide(a, b) {
 function operate(op, a, b) {
     switch (op) {
         case '+':
-            add(a, b);
-            break;
+            return add(a, b);
         case '-':
-            subtract(a, b);
-            break;
+            return subtract(a, b);
         case '*':
-            multiply(a, b);
-            break;
+            return multiply(a, b);
         case '/':
-            divide(a, b);
-            break;
+            return divide(a, b);
         default:
-            break;
+            return NaN;
     }
 }
 
-const display = document.querySelector('.display');
 let displayValue = '';
+let a, b, op;
+
+const displayDiv = document.querySelector('.display');
 const digitButtons = document.querySelectorAll('.digit');
 [...digitButtons].forEach(b => b.addEventListener('click', e => {
     displayValue += e.target.innerText;
-    display.innerText = displayValue;
-}))
+    displayDiv.innerText = displayValue;
+}));
+
+const operatorButtons = document.querySelectorAll('.operator');
+[...operatorButtons].forEach(b => b.addEventListener('click', e => {
+    if (!Number.isFinite(a)) {
+        op = e.target.innerText;
+        a = +displayValue;
+        if (isNaN(a)) {
+            displayValue = 'Error';
+            displayDiv.innerText = displayValue;
+            return;
+        }
+
+        displayValue = '';
+        displayDiv.innerText = displayValue;
+    }
+    else
+    {
+        b = +displayValue;
+        if (isNaN(b)) {
+            displayValue = 'Error';
+            displayDiv.innerText = displayValue;
+            return;
+        }
+
+        a = operate(op, a, b);
+        op = e.target.innerText;
+
+        displayValue = '';
+        displayDiv.innerText = a;
+    }
+
+}));
+
+document.getElementById('clearButton').addEventListener('click', () => {
+    displayDiv.innerText = '';
+    displayValue = '';
+    op = '';
+    a = null;
+    b = null;
+});
+
+document.getElementById('equalButton').addEventListener('click', () => {
+    if (isNaN(a)) {
+        displayValue = 'Error';
+        displayDiv.innerText = displayValue;
+        return;
+    }
+
+    b = +displayValue;
+    displayValue = '' + operate(op, a, b);
+    displayDiv.innerText = displayValue;
+    a = null;
+    b = null;
+});
+
+document.getElementById('deleteButton').addEventListener('click', () => {
+    displayValue = displayValue.slice(0, displayValue.length - 1);
+    displayDiv.innerText = displayValue;
+});
